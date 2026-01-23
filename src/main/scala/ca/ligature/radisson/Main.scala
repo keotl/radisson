f105e2a0b99a5ef8a87dd4f8bd6f2635e17362af
Kento Lauzon <kento@lauzon.co.uk>
2026-01-23 08:00:43 +0000

    cleanup

--- src/main/scala/ca/ligature/radisson/Main.scala
+++
@@ -1,8 +0,0 @@
-package ca.ligature.radisson
-
-import ca.ligature.radisson.actor.root.RootSupervisor
-import org.apache.pekko.actor.typed.ActorSystem
-
-@main def main(): Unit = {
-  ActorSystem[Nothing](RootSupervisor(), "radisson-system")
-}
